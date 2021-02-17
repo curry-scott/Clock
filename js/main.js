88c31ba2346dd525e2e7ff55ae459ffcdbc8dd22
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-17 13:46:07 -0500

    index.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let clock = document.getElementById('clock');
+<<<<<<< HEAD
 let militaryTime = false;
 
 const convertToTwelve = document.querySelector('.twelveButton');
@@ -9,12 +10,29 @@ convertToTwentyFour.addEventListener('click', convertHourToMilitary);
 
  function convertHourToMilitary() {
 
+=======
+const convertToTwelve = document.querySelector('.twelveButton');
+const convertToTwentyFour = document.querySelector('.twenty-fourButton');
+convertToTwelve.addEventListener('click', convertHourDefault);
+convertToTwentyFour.addEventListener('click', convertHourToMilitary);
+
+let militaryTime = false;
+
+ function convertHourToMilitary() {
+>>>>>>> a4010e0a8acf44a286aaf5ea10670ff5a79f3663
     if (militaryTime === false) {
         militaryTime = true;
 
     }
 }
+function convertHourDefault() {
+    if (militaryTime === true) {
+        militaryTime = false;
+    }
 
+}
+
+<<<<<<< HEAD
 function convertHourDefault() {
     if (militaryTime === true) {
         militaryTime = false;
@@ -26,6 +44,12 @@ setInterval(displayClock, 10);
 
 function displayClock() {
     
+=======
+setInterval(displayClock, 10);
+
+function displayClock() {
+
+>>>>>>> a4010e0a8acf44a286aaf5ea10670ff5a79f3663
     let currentTime = new Date();
 
     let hoursin24 = currentTime.getHours();
@@ -42,12 +66,18 @@ function displayClock() {
     }
   
     if (hoursin24 > 12 & militaryTime === false) {
+<<<<<<< HEAD
         
+=======
+>>>>>>> a4010e0a8acf44a286aaf5ea10670ff5a79f3663
         hoursin24 = (24 - hoursin24) - 12;
     }
 
     hoursin12 = Math.abs(hoursin24);
+<<<<<<< HEAD
   
+=======
+>>>>>>> a4010e0a8acf44a286aaf5ea10670ff5a79f3663
     if (hoursin12 < 10) {
         hoursin12 = '0' + hoursin12;
     }
