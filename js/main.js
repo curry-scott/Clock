d97179ebc7374cf00521ba40284b7a13b281b802
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-17 13:36:17 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,46 +1,67 @@
-let clock = document.getElementById('clock')
-    const convertToTwelve =
-document.querySelector('.twelveButton');
-    const convertToTwentyFour = 
-document.querySelector('twento-fourButton');
+let clock = document.getElementById('clock');
 let militaryTime = false;
 
-function convertHourToMilitary() {
-    if (militaryTime === falso) {
+const convertToTwelve = document.querySelector('.twelveButton');
+const convertToTwentyFour = document.querySelector('.twenty-fourButton');
+
+convertToTwelve.addEventListener('click', convertHourDefault);
+convertToTwentyFour.addEventListener('click', convertHourToMilitary);
+
+ function convertHourToMilitary() {
+
+    if (militaryTime === false) {
         militaryTime = true;
+
     }
 }
 
-setInterval(displayClock, 1000);
+function convertHourDefault() {
+    if (militaryTime === true) {
+        militaryTime = false;
+    }
+
+}
+
+setInterval(displayClock, 10);
 
 function displayClock() {
+    
     let currentTime = new Date();
+
     let hoursin24 = currentTime.getHours();
+
     let minutes = currentTime.getMinutes();
     let seconds = currentTime.getSeconds();
-    let amORPm = '';
+    let amOrPm = '';
+
     if (hoursin24 < 12) {
-        amORPm = 'AM'
+        amOrPm = 'AM';
     }
     else{
-        amORPm = 'PM'
+        amOrPm = 'PM';
     }
-    if (hoursin24 >12 & militaryTime === false) {
+  
+    if (hoursin24 > 12 & militaryTime === false) {
+        
         hoursin24 = (24 - hoursin24) - 12;
-        }
-
-        hoursin12 = Math.abs(hoursin24);
-        if (hoursin12 <10) {
-            hoursin12 = '0' + hoursin12;
-        }
-        if (minutes <10) {
-            minutes = '0' + minutes;
-        }
-        if (seconds < 10) {
-            seconds = '0' + seconds; 
-        }
-        let timeNow = hoursin12 + ':' + minutes + ':' + seconds + amORPm;
-        clock.innerHTML = timeNow;
     }
 
-    displayClock();
+    hoursin12 = Math.abs(hoursin24);
+  
+    if (hoursin12 < 10) {
+        hoursin12 = '0' + hoursin12;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    let timeNow = hoursin12 + ':' + minutes + ':' + seconds + amOrPm;
+    
+    clock.innerHTML = timeNow;
+    
+}
+
+displayClock();
