d405bae0831886ccbd215801725bb489fc5dd5c5
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-17 12:04:35 -0500

    updated js to hopefully show clock

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
-const clock = document.getElementById('clock');
-const time = document.getElementById('clock');
-const btn = document.querySelector('button');
-const b12 = document.getElementById('12');
-const b24 = document.getElementById('24');
+let militaryTime = false;
+let clock = document.getElementById('clock')
+const convertToTwelve =
+document.querySelector('.twelveButton');
+const convertToTwentyFour = 
+document.querySelector('twento-fourButton');
 
+function convertHourToMilitary() {
+    if (militaryTime === falso) {
+        militaryTime = true;
+    }
+}
 
+setInterval(displayClock, 1000);
 
+function displayClock() {
+    let currentTime = new Date();
+    let hoursin24 = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let amORPm = '';
+    if (hoursin24 >= 12) {
+        amORPm = 'PM'
+    }
+    else{
+        amORPm = 'AM'
+    }
+    if (hoursin24 >12 & militaryTime === false) {
+        hoursin24 = (24 - hoursin24) - 12;
+        }
 
+        hoursin12 = Math.abs(hoursin24);
+        if (hoursin12 <10) {
+            hoursin12 = '0' + hoursin12;
+        }
+        if (minutes <10) {
+            minutes = '0' + minutes;
+        }
+        if (seconds < 10) {
+            seconds = '0' + seconds; 
+        }
+        let timeNow = hoursin12 + ':' + minutes + ':' + seconds + amORPm;
+        clock.innerHTML = timeNow;
+    }
 
-function showTime() { 
-    let time = new Date(); 
-    let hour = time.getHours(); 
-    let min = time.getMinutes(); 
-    let sec = time.getSeconds(); 
-    am_pm = "AM"; 
+    displayClock();
